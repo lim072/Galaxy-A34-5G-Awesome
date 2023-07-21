fcc9bab2c091e7b7b4ca5ed22324f56af1dedba0
lim072 <bestbro072@gmail.com>
2023-07-21 15:37:52 +0900

    scroll 수정

--- script/scroll.js
+++ script/scroll.js
@@ -1,17 +1,12 @@
 let sections = document.querySelectorAll("body>section")
-let sectionArr = Array.from(sections);
 let header = document.querySelector("header");
 let btns = header.querySelector(".btns")
-let moreCons = document.querySelectorAll("#contentMore>article")
-let moreConArr = Array.from(moreCons);
-const moreBtn = document.querySelectorAll(".frame .on a");
-console.log(moreBtn)
+
 
 let posArr = [];
-let morePosArr = [];
 
 setPos();
-setPos2();
+
 function setPos() {
     posArr = [];
     for (let el of sections) {
@@ -22,32 +17,11 @@ function setPos() {
 //[0, 968, 3198, 8038, 12700, 13628]
 
 
-function setPos2() {
-    morePosArr = [];
-    for (let el of moreCons) {
-        morePosArr.push(el.offsetTop);
-    }
-}
-// console.log(morePosArr);
-//[3198, 4166, 5134, 6102, 7070]
-
-
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset < 800 || window.pageYOffset > 3146) {
+    if (window.pageYOffset < 800 || window.pageYOffset > 3100) {
         btns.style.display = "none";
     } else {
         btns.style.display = "block";
     }
 })
 
-moreBtn.forEach((el, index) => {
-    el.addEventListener("click", (e) => {
-        e.preventDefault();
-        moveScroll(index)
-    })
-})
-
-
-function moveScroll(index) {
-    window.scrollTo({ top: morePosArr[index], behavior: "smooth" })
-}
