e102ab990b3086383015d01aed524994a7088929
lim072 <bestbro072@gmail.com>
2023-07-25 17:25:38 +0900

    scroll 이벤트 추가

--- script/scroll.js
+++ script/scroll.js
@@ -1,7 +1,7 @@
 let sections = document.querySelectorAll("body>section")
 let header = document.querySelector("header");
-let btns = header.querySelector(".btns")
-
+let btns = header.querySelector(".btns");
+// const tablet = window.matchMedia("screen and (max-width : 800px)");
 
 let posArr = [];
 
@@ -23,5 +23,40 @@ window.addEventListener("scroll", () => {
     } else {
         btns.style.display = "block";
     }
+
+    const con1 = document.querySelectorAll('.con1');
+    const trigger1 = 1100;
+    const con2 = document.querySelectorAll('.con2');
+    const trigger2 = 1150;
+    const con3 = document.querySelectorAll('.con3');
+    const trigger3 = 1200;
+    const con4 = document.querySelectorAll('.con4');
+    const trigger4 = 1400;
+    const con5 = document.querySelectorAll('.con5');
+    const trigger5 = 1450;
+    const con6 = document.querySelectorAll('.con6');
+    const trigger6 = 1600;
+    const con7 = document.querySelectorAll('.con7');
+    const trigger7 = 1700;
+    const con8 = document.querySelectorAll('.con8');
+    const trigger8 = 1650;
+    const num = 1;
+    const con = [con1,con2,con3,con4,con5,con6,con7,con8];
+
+    
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition)
+
+
+    for(let index of con){
+        for(let el of index){
+            if(scrollPosition > trigger1){
+                el.style.opacity = 1;
+            }else{
+                el.style.opacity = 0.2;
+            }
+        }
+    }
+
 })
 
